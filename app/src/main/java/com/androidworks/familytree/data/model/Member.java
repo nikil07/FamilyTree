0998ea436976268adfc7658728a43041682f7edc
nikil07 <nikilkumar0810@gmail.com>
2018-07-05 20:36:02 +0530

    Added models and shit

---
+++ app/src/main/java/com/androidworks/familytree/data/model/Member.java
@@ -0,0 +1,41 @@
+package com.androidworks.familytree.data.model;
+
+public class Member {
+
+    private String name;
+    private String nickName;
+    private int age;
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
--- app/src/main/java/com/androidworks/familytree/ui/activities/MainActivity.java
+++ app/src/main/java/com/androidworks/familytree/ui/activities/MainActivity.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,62 +33,77 @@ public class MainActivity extends AppCompatActivity {
     TextView table;
     @BindView(R.id.bt_show)
     Button show;
+    @BindView(R.id.bt_add)
+    Button add;
+    SQLiteDatabase writableDB;
+    SQLiteDatabase readableDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         FamilyDbHelper dbHelper = new FamilyDbHelper(this);
 
         // Gets the data repository in write mode
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        writableDB = dbHelper.getWritableDatabase();
+        readableDB = dbHelper.getReadableDatabase();
 
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 // Create a new map of values, where column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(FamilyContract.FamilyTable.COLUMN_NAME, name.getText().toString());
-        values.put(FamilyContract.FamilyTable.COLUMN_AGE, age.getText().toString());
+                ContentValues values = new ContentValues();
+                values.put(FamilyContract.FamilyTable.COLUMN_NAME, name.getText().toString());
+                values.put(FamilyContract.FamilyTable.COLUMN_AGE, age.getText().toString());
 
 // Insert the new row, returning the primary key value of the new row
-        long newRowId = db.insert(FamilyContract.FamilyTable.TABLE_NAME, null, values);
-        Log.d(TAG, "DB row ID " + newRowId);
-
-
-        // Define a projection that specifies which columns from the database
-// you will actually use after this query.
-        String[] projection = {
-                BaseColumns._ID,
-                FamilyContract.FamilyTable.COLUMN_NAME,
-                FamilyContract.FamilyTable.COLUMN_AGE
-        };
-
-// Filter results WHERE "title" = 'My Title'
-        String selection = FamilyContract.FamilyTable.COLUMN_NAME + " = ?";
-        String[] selectionArgs = {"My Title"};
-
-// How you want the results sorted in the resulting Cursor
-        String sortOrder =
-                FamilyContract.FamilyTable.COLUMN_AGE + " DESC";
-
-        final Cursor cursor = db.query(
-                FamilyContract.FamilyTable.TABLE_NAME,   // The table to query
-                projection,             // The array of columns to return (pass null to get all)
-                selection,              // The columns for the WHERE clause
-                selectionArgs,          // The values for the WHERE clause
-                null,                   // don't group the rows
-                null,                   // don't filter by row groups
-                sortOrder               // The sort order
-        );
+                long newRowId = writableDB.insert(FamilyContract.FamilyTable.TABLE_NAME, null, values);
+                Log.d(TAG, "DB row ID " + newRowId);
+            }
+        });
 
         show.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 List itemIds = new ArrayList<>();
+
+                // Define a projection that specifies which columns from the database
+// you will actually use after this query.
+                String[] projection = {
+                        BaseColumns._ID,
+                        FamilyContract.FamilyTable.COLUMN_NAME,
+                        FamilyContract.FamilyTable.COLUMN_AGE
+                };
+
+// Filter results WHERE "title" = 'My Title'
+                String selection = FamilyContract.FamilyTable.COLUMN_NAME + " = ?";
+                String[] selectionArgs = {"My Title"};
+
+// How you want the results sorted in the resulting Cursor
+                String sortOrder =
+                        FamilyContract.FamilyTable.COLUMN_AGE + " DESC";
+
+                final Cursor cursor = readableDB.query(
+                        FamilyContract.FamilyTable.TABLE_NAME,   // The table to query
+                        projection,             // The array of columns to return (pass null to get all)
+                        null,              // The columns for the WHERE clause
+                        null,          // The values for the WHERE clause
+                        null,                   // don't group the rows
+                        null,                   // don't filter by row groups
+                        sortOrder               // The sort order
+                );
+
                 while (cursor.moveToNext()) {
                     long itemId = cursor.getLong(
                             cursor.getColumnIndexOrThrow(FamilyContract.FamilyTable._ID));
                     itemIds.add(itemId);
+                    Log.d(TAG, "Name " + cursor.getString(1));
+                    Log.d(TAG, "Age " + cursor.getString(2));
                 }
+
+                table.setText(itemIds.toString());
                 cursor.close();
             }
         });
